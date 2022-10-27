aa71e1ef007744d6450a063326b33e5fb503c7f1
SpicyRiceTea <chaoukirami@live.ca>
2022-10-26 22:29:42 -0400

    idk what this is anymore

---
+++ src/main/java/com/data/configurations/WebSecurityConfig.java
@@ -0,0 +1,62 @@
+package com.data.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.data.service.CustomUserDetailsService;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration {
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new CustomUserDetailsService();
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService());
+		authProvider.setPasswordEncoder(passwordEncoder());
+		return authProvider;
+	}
+
+	
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(authenticationProvider());
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+		http.authorizeRequests((authorize)->authorize
+				.antMatchers("/products/**").hasAnyRole("USER")
+				.antMatchers("/admin/**").hasAnyRole("ADMIN")
+				.antMatchers("/", "/error**" ,"/login", "/users/**", "/images/**", "/css/**", "/js/**").permitAll()
+				.anyRequest().authenticated())
+		.formLogin((formLogin)->formLogin
+				.loginPage("/login")
+				.defaultSuccessUrl("/", true).permitAll())
+		.logout((logout)->logout.
+				invalidateHttpSession(true)
+				.clearAuthentication(true)
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+				.logoutSuccessUrl("/")
+				.permitAll());
+		
+		return http.build();
+	}
+}
---
+++ src/main/java/com/data/repository/ProductRepository.java
@@ -0,0 +1,34 @@
+package com.data.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.data.model.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+//	@Query("Select p FROM Product p WHERE p.user.id = ?1")
+//	public List<Product> findByUserIdRami(long userId);
+	
+	public Optional<Product> findById(Long id);
+
+	@Query("SELECT p FROM Product p Where (onLoan = false) OR (onLoan = null)")
+	public List<Product> findAllProductsNotOnLoan();
+	
+//	@Query("Select p FROM Product p WHERE p.user_id = ?1")
+	@Query(value="select * from products where user_id=?1", nativeQuery=true)
+	public List<Product> findByUserId(Long userId);
+	
+	@Modifying
+//	@Query("DELETE FROM Product p WHERE p.id = ?1")
+	public void deleteById(Long id);
+	
+	@Query("UPDATE Product p SET p.id = ?1")
+	public void setOnLoanStatus(Long id);
+}
